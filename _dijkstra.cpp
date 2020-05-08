60f45ee12eaf732df6391656a2d4ddec98764f82
kayaru28 <istorytale090415@gmail.com>
2020-05-09 01:35:26 +0900

    update

--- _dijkstra.cpp
+++ _dijkstra.cpp
@@ -1,49 +1,72 @@
-//https://nexusuica.hatenablog.jp/entry/2018/08/14/114524
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+using namespace std;
+#include <vector>
+#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
 
-struct edge{ll to, cost;};
+
+
+
+
+//https://nexusuica.hatenablog.jp/entry/2018/08/14/114524
+#include <queue>
+
+struct edge{ll next_index, cost;};
 typedef pair<ll,ll> P;
 struct graph{
-    ll V;
+    ll graph_size;
     vector<vector <edge> > G;
-    vector<ll> d;
-
+    vector<ll> node_costs;
     graph(ll n){
         init(n);
     }
     void init(ll n){
-        V = n;
-        G.resize(V);
-        d.resize(V);
-        REP(i,V){
-            d[i] = INF;
+        graph_size = n;
+        G.resize(graph_size);
+        node_costs.resize(graph_size);
+        rep(i,graph_size){
+            node_costs[i] = INF;
         }
     }
 
-    void add_edge(ll s, ll t, ll cost){
+    void add_edge(ll start_index, ll next_index, ll cost){
         edge e;
-        e.to = t, e.cost = cost;
-        G[s].push_back(e);
+        e.next_index = next_index;
+        e.cost = cost;
+        G[start_index].push_back(e);
     }
-    void dijkstra(ll s){
-        REP(i,V){
-            d[i] = INF;
+    void dijkstra(ll base_index){
+        rep(i,graph_size){
+            node_costs[i] = INF;
         }
-        d[s] = 0;
+        node_costs[base_index] = 0;
         priority_queue<P,vector<P>, greater<P> > que;
-        que.push(P(0,s));
+        que.push(P(0,base_index));
         while(!que.empty()){
-            P p = que.top(); que.pop();
-            ll v = p.second;
-            if(d[v]<p.first) continue;
-            for(auto e : G[v]){
-                if(d[e.to]>d[v]+e.cost){
-                    d[e.to] = d[v]+e.cost;
-                    que.push(P(d[e.to],e.to));
+            P p = que.top();
+            que.pop();
+            ll index = p.second;
+            ll updated_cost = p.first;
+            if(node_costs[index] < updated_cost) continue;
+            for(auto e : G[index]){
+                if(node_costs[e.next_index] > node_costs[index]+e.cost){
+                    node_costs[e.next_index] = node_costs[index]+e.cost;
+                    que.push(P(node_costs[e.next_index],e.next_index));
                 }
             }
         }
     }
 };
+/*
 graph g(n);
 //と書けばn頂点のグラフが作成され、
 g.add_edge(a,b,cost);
@@ -51,3 +74,21 @@ g.add_edge(a,b,cost);
 g.dijkstra(s);
 //とすれば頂点sから各頂点への最短経路をダイクストラ法で探索できます。探索結果は
 int dist = g.d[i]:
+*/
+
+
+int main(){
+    ll N=10;
+    graph g(N);
+    rep(ni,N-1){
+        g.add_edge(ni,ni+1,1);
+        g.add_edge(ni+1,ni,1);
+    }
+    cout << "start" << endl;
+    g.dijkstra(2);
+    rep(ni,N) cout << ni << "\t: " << g.node_costs[ni] << endl;
+
+}
+
+
+
