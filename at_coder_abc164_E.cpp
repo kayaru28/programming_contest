5f5e8069d97f6392039f76a150b8a61231719d3b
kayaru28 <istorytale090415@gmail.com>
2020-05-12 22:34:17 +0900

    add

--- at_coder_abc164_E.cpp
+++ at_coder_abc164_E.cpp
@@ -12,6 +12,8 @@ using namespace std;
 #define ull unsigned long long
 #define vint vector<int>
 #define vll vector<ll>
+#include <queue>
+#include <queue>
 
 //#include <stack>
 //#include <queue>
@@ -30,82 +32,164 @@ ll M;
 ll S;
 vll candidate;
 ll cost[55]={};
+ll silver[55]={};
 vll neighber[110];
-vll neighber_time[110];
-vll neighber_cost[110];
 ll C[55];
 ll D[55];
 ll confirm[55]={};
 
-class dfind {
-public:
-    cost[N+10]={};
-    silber[N+10]={};
-    void dsearch(int index){
-        rep(ni,neighber[index].size()){
-            int nei = neighber[index][ni];
-            if(cost[index]+neighber_cost[index][ni]<cost[nei]){
-                cost[nei] = cost[index]+neighber_cost[index][ni];
-                dsearch(nei);
-            }
+struct edge{ll to, cost,silv;};
+typedef pair<ll,ll> P;
+ll buySilver(edge e,ll base_index,ll base_silv);
+struct graph{
+    ll V;
+    vector<vector <edge> > G;
+    vector<ll> d;
+    vector<ll> silv;
+    
+    graph(ll n){
+        init(n);
+    }
+    void init(ll n){
+        V = n;
+        G.resize(V);
+        d.resize(V);
+        silv.resize(V);
+        rep(i,V){
+            d[i] = INF;
+            silv[i] = 0;
         }
     }
-    dfind(int index,ll base_cost,ll silver){ //最初は全てが根であるとして初期化
-        rep(ni,N+10) cost[ni]=llINF
-        cost[index]=0;
-        dsearch(index);
-        for( int ni = 1 ; ni <=N ; ni++ ){
-            if(confirm[ni]!=1){
-                if(cost[ni]<cost[])
-            }
-        }
 
+    void add_edge(ll s, ll t, ll cost,ll silv){
+        edge e;
+        e.to = t, e.cost = cost;
+        e.silv = silv;
+        G[s].push_back(e);
     }
-}
+    void dijkstra(ll s){
+        rep(i,V){
+            d[i] = INF;
+        }
+        d[s] = 0;
+        silv[s] = silver[s];
+        ll unit_C = C[s];
+        ll unit_D = D[s];
+        priority_queue<P,vector<P>, greater<P> > que;
+        que.push(P(0,s));
+        while(!que.empty()){
+            P p = que.top(); que.pop();
+            ll v = p.second;
+            if(d[v]<p.first) continue;
+            for(auto e : G[v]){
+                ll buy = buySilver(e,s,silv[v]);
+                ll cost_i = d[v]+e.cost+buy*unit_D;
+                if(d[e.to]>cost_i){
+                    d[e.to] = cost_i;
+                    silv[e.to] = silv[v]+buy*unit_C-e.silv;
+                    que.push(P(d[e.to],e.to));
+                }
+            }
+        }
+    }
+};
+/*
+graph g(n);
+//と書けばn頂点のグラフが作成され、
+g.add_edge(a,b,cost);
+c//とすれば頂点aから頂点bへのコストcostの辺が張られます。そして
+g.dijkstra(s);
+//とすれば頂点sから各頂点への最短経路をダイクストラ法で探索できます。探索結果は
+int dist = g.d[i]:
+*/
 
 void doneConfirm(int index){
     confirm[index]=1;
-    rep(ni,neighber[index].size()){
+    int Vsize = neighber[index].size();
+    rep(ni,Vsize){
         int next_index = neighber[index][ni];
         if(confirm[next_index]!=1){
             candidate.push_back(neighber[index][ni]);
          }
     }
-    sort(cadidate.begin(), cadidate.end());
-    decltype(cadidate)::iterator result = std::unique(cadidate.begin(), cadidate.end());
-    cadidate.erase(result, cadidate.end());
-
-
+    sort(candidate.begin(), candidate.end());
+    decltype(candidate)::iterator result = std::unique(candidate.begin(), candidate.end());
+    candidate.erase(result, candidate.end());
+}
+ll buySilver(edge e,ll base_index,ll base_silv){
+    ll buy;
+    ll need;
+    need = e.silv-base_silv;
+    if(need>0){
+        if(need % C[base_index] ==0){
+            buy = need / C[base_index];
+        }else{
+            buy = need / C[base_index] + 1;
+        }
+    }else{
+        buy = 0;
+    }
+    return buy;
+}
+void update(graph g,ll base_index){
+    rep(ni,N){
+        
+    }
 
 }
-
 int main(){
 
     cin >> N >> M >> S;
 
+    graph g(N);
 
-    int U,V,A,B;
+    ll U,V,A,B;
     rep(mi,M){
         cin >> U;
         cin >> V;
         cin >> A;
         cin >> B;
+        U--;
+        V--;
         neighber[U].push_back(V);
-        neighber_cost[U].push_back(A);
-        neighber_time[U].push_back(B);
         neighber[V].push_back(U);
-        neighber_cost[V].push_back(A);
-        neighber_time[V].push_back(B);
+        g.add_edge(U,V,B,A);
+        g.add_edge(V,U,B,A);
     }
     rep(ni,N){
-        cin >> C[ni+1];
-        cin >> D[ni+1];
+        cin >> C[ni];
+        cin >> D[ni];
+        cost[ni]=llINF;
     }
-    int index = 1;
+    int index = 0;
+    cost[index]=0;
 
+    g.dijkstra(index);
+    doneConfirm(index);
+    update(g,0);
     while(candidate.size()!=0){
-
+        ll index_i;
+        ll cand_i;
+        cand_i = 0;
+        index = candidate[cand_i];
+        int Vsize = candidate.size();
+        rep(ci,Vsize){
+            index_i= candidate[ci];
+            if(cost[index]>cost[index_i]){
+                cand_i = ci;
+                index = index_i;
+            }
+        }
+        candidate.erase(candidate.begin()+cand_i);
+        //cout << "target " << index << endl;
+        //rep(ni,N) cout << "cost " << ni << " : " << cost[ni] << endl;
+        //rep(ci,candidate.size()) cout << "candi : " << candidate[ci] << endl;
+        doneConfirm(index);
+        S = silver[index];
+        g.dijkstra(index);
+        update(g,index);
     }
-
-
+    for( int ni = 1 ; ni <N ; ni++ ){
+        cout << cost[ni] << endl;
+    }
 }
