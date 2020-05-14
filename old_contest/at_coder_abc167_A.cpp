5f1ee6f1ca8008c5b462bfb6a22f70ac1ba7cd59
kayaru28 <istorytale090415@gmail.com>
2020-05-14 23:41:42 +0900

    add 167 A

---
+++ old_contest/at_coder_abc167_A.cpp
@@ -0,0 +1,52 @@
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+using namespace std;
+#include <vector>
+#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
+//#include <stack>
+//#include <queue>
+
+/*
+#include <math.h>
+int standerd = int(pow(10.0,9.0)) + 7;
+*/
+string ans_Yes = "Yes"; 
+string ans_No = "No"; 
+string ans_yes = "yes"; 
+string ans_no = "no"; 
+
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
+
+int main(){
+
+    
+    string S;
+    string T;
+    
+    cin >> S;
+    cin >> T;
+
+    if(S.length()==T.length()-1&&T.substr(0,S.length())==S){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+    
+
+}
