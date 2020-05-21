f9fa7b6370c149ec492ccddd039f486c8a2da469
kayaru28 <istorytale090415@gmail.com>
2020-05-21 23:45:02 +0900

    update

--- at_coder_abc1XX_.cpp
+++ at_coder_abc1XX_.cpp
@@ -1,13 +1,20 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
 #define rep(i,n) for (int i = 0; i < (n) ; i++)
 #define INF 1e9
 #define llINF 1e18
-#define base10_5 1e5 //100000
-#define base10_6 1e6 //1000000
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
 
 #define MOD 1000000007
 #define pb push_back
@@ -19,10 +26,6 @@ using namespace std;
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
