8f849a50f1b2101c7b67b6b5d4337b11b622b624
kayaru28 <istorytale090415@gmail.com>
2020-06-06 23:11:43 +0900

    make tool

---
+++ __provision.bat
@@ -0,0 +1,8 @@
+set /P CONTEST="input contest(ex.abc170): "
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_A.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_B.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_C.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_D.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_E.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_F.cpp
+set /P USR_INPUT_STR="finished(input something): "
