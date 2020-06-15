58032a3ec00bf4e750766f4e1a5991bdf21f6cdb
kayaru28 <istorytale090415@gmail.com>
2020-06-16 00:28:44 +0900

    add MARINE B

---
+++ old_contest/at_coder_MARINE2020_B.cpp
@@ -0,0 +1,70 @@
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+
+using namespace std;
+#include <vector>
+#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
+//#include <stack>
+//#include <queue>
+
+string ans_Yes = "Yes"; 
+string ans_No = "No"; 
+string ans_yes = "yes"; 
+string ans_no = "no"; 
+
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll V,W;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> A;
+    cin >> V;
+
+    cin >> B;
+    cin >> W;
+
+    cin >> C;
+
+    if(V<=W) cout << "NO" << endl;
+    else{
+        ll diff =  abs(A-B);
+        ll diff_v = abs(V-W);
+        ll time = (diff-1) / diff_v;
+        time++;
+        if(time <= C) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    
+
+}
