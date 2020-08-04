d6fec012046d7ba1b6a26ceee0b24a8d8fba6002
kayaru28 <istorytale090415@gmail.com>
2020-08-05 08:30:56 +0900

    update abc126 B

--- old_contest/at_coder_abc126_B.cpp
+++ old_contest/at_coder_abc126_B.cpp
@@ -1,51 +1,91 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
-//#include <vector>
+#include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
-#include <string>
 
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
 
-int getIntSubstr(string s,int start_index,int length){
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll getllSubstr(string s,ll start_index,ll length){
     return atoi(s.substr(start_index,length).c_str());
 }
-  
+
+
 int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     string S;
     cin >> S;
 
-    int tmp;
-    
-    tmp = getIntSubstr(S,0,2);
-    if(tmp>0 and tmp < 13){
-        tmp = getIntSubstr(S,2,2);
-        if(tmp>0 and tmp < 13){
-            cout << "AMBIGUOUS";
-        }else{
-            cout << "MMYY";
-        }
-        
-    }else{
-        tmp = getIntSubstr(S,2,2);
-        if(tmp>0 and tmp < 13){
-            cout << "YYMM";
-        }else{
+    bool flagYYMM=true;
+    bool flagMMYY=true;
 
-            cout << "NA";
+    ll head2 = getllSubstr(S,0,2);
+    ll tail2 = getllSubstr(S,2,2);
+    if(tail2==0) flagYYMM = false;
+    if(head2==0) flagMMYY = false;
+    if(head2>12) flagMMYY = false;
+    if(tail2>12) flagYYMM = false;
+
+
+    if(flagYYMM){
+        if(flagMMYY){
+            cout << "AMBIGUOUS" << endl;
+        }else{
+            cout << "YYMM" << endl;
+        }
+    }else{
+        if(flagMMYY){
+            cout << "MMYY" << endl;
+
+        }else{
+            cout << "NA" << endl;
+            
         }
 
     }
+    
 
 }
