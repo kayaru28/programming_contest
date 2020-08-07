206c8c6593e933f8d70d421cd33c0c5f79353b4e
kayaru28 <istorytale090415@gmail.com>
2020-08-08 00:58:20 +0900

    add abc128 C

--- old_contest/at_coder_abc128_C.cpp
+++ old_contest/at_coder_abc128_C.cpp
@@ -1,81 +1,101 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
-//#include <vector>
+#include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
 
-int countSwitch(int sw[],int status[][10],int N, int ni,int P[],int M){
-    if(ni >= N){
-//        cout << "count:" << ni << endl;
-        for( int mi = 0 ; mi < M ; mi++ ){
-            int sumv = 0;
-            for( int nni = 0 ; nni < N ; nni++ ){
-                sumv+= status[mi][nni] * sw[nni];
-//                cout << sw[nni];
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll ans=0;
+int flag[20][20]={};
+int p[20]={};
+void getans(ll pt[20],ll index){
+    if(index==N){
+        bool fg=true;
+        rep(mi,M){
+            ll count=0;
+            rep(ni,N){
+                count += flag[mi][ni] * pt[ni];
             }
-//            cout << endl;
-//            cout << sumv << endl;
-            if(sumv%2!=P[mi]){
-                return 0;
-            }
+            if(count%2!=p[mi]){
+                fg = false;
+                break;
+            } 
         }
-        return 1;
+        if(fg) ans++;
     }else{
-        int count;
-        sw[ni]=0;
-        count = countSwitch(sw,status,N,ni+1,P,M);
-        sw[ni]=1;
-        count += countSwitch(sw,status,N,ni+1,P,M);
-        return count;
+        pt[index]=1;
+        getans(pt,index+1);
+        pt[index]=0;
+        getans(pt,index+1);
     }
+
 }
 
 int main(){
 
-    int N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     cin >> N;
-
-    int M;
     cin >> M;
-    int k[M];
-    int status[10][10]={};
-
-    for( int mi = 0 ; mi < M ; mi++ ){
-        cin >> k[mi];
-        int val;
-        for( int si = 0 ; si < k[mi] ; si++ ){
-            cin >> val;
-            status[mi][val-1]=1;
+    ll pt[20] = {};
+    rep(mi,M){
+        cin >> K;
+        rep(ki,K){
+            int sw;
+            cin >> sw;
+            sw--;
+            flag[mi][sw]=1;
         }
     }
-
-    int P[M];
-    for( int pi = 0 ; pi < M ; pi++ ){
-        cin >> P[pi];
+    rep(mi,M){
+        int pp;
+        cin >> pp;
+        p[mi]=pp;
     }
 
-    int count=0;
-    int sw[N];
 
-    cout << countSwitch(sw,status,N,0,P,M);
-    
-
-
-
-    
-
-
+    getans(pt,0);
+    cout << ans << endl;
 }
