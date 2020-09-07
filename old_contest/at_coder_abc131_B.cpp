cfdc42693a71e989ee9e1f64f108f6710b1a5f26
kayaru28 <istorytale090415@gmail.com>
2020-09-07 23:23:33 +0900

    update abc131 B

--- old_contest/at_coder_abc131_B.cpp
+++ old_contest/at_coder_abc131_B.cpp
@@ -1,39 +1,74 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
+
+vll A;
+vll B;
+ll C;
+ll N;
+ll L;
+ll K;
+
+ll ltmp;
+string stmp;
+double dtmp;
 
 int main(){
 
-    int N;
-    int L;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> N;
     cin >> L;
 
-    int sum=0;
-    int eaten;
-
-    eaten = L + 1 -1;
-    for( int ni = 0 ; ni < N ; ni++ ){
-        sum+=L+(ni+1)-1;
-        if(abs(L+(ni+1)-1) < abs(eaten)) eaten = L+(ni+1)-1;
+    ll sumA=0;
+    rep(ni,N){
+        ltmp = L+(ni+1)-1;
+        A.push_back(ltmp);
+        sumA+=ltmp;
     }
-    //cout << sum << endl;
-    //cout << eaten << endl ;
-    sum -= eaten;
-    cout << sum << endl;
+    
+    ll ans = sumA;
+    if(A[0]>0) ans-=A[0];
+    else if(A[N-1]<0) ans-=A[N-1];
+    
+    cout << ans << endl;
+
+
+    
 
 }
