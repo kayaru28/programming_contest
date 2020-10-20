fec60b64bdf94c015ff561921016ae2a90f1c5e0
kayaru28 <istorytale090415@gmail.com>
2020-10-21 00:43:30 +0900

    update abc 132 D

--- old_contest/at_coder_abc132_D.cpp
+++ old_contest/at_coder_abc132_D.cpp
@@ -1,14 +1,25 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
-#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
 #define INF 1e9
 #define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
 #define MOD 1000000007
 #define pb push_back
 #define ll long long
+#define ld long double
 #define ull unsigned long long
 #define vint vector<int>
 #define vll vector<ll>
@@ -16,48 +27,73 @@ using namespace std;
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
 
-int N,K;
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
 
-vector<vector<long long>> combination(int n, int r, int mod=0) {
-    vector<vector<long long>> v(n + 1,vector<long long>(n + 1, 0));
-    for (int i = 0; i < v.size(); i++) {
-        v[i][0] = 1;
-        v[i][i] = 1;
+ll ltmp;
+string stmp;
+double dtmp;
+
+vector<ll> inv,fact,invfact;
+void setupCombination(ll n_max,ll mod){
+    fact.resize(n_max+1);
+    inv.resize(n_max+1);
+    invfact.resize(n_max+1);
+    fact[0]=inv[0]=invfact[0]=1;
+    inv[1]=1;
+    for(ll i=0;i<n_max;i++){
+        fact[i+1]=fact[i]*(i+1)%mod;
+        if(i>0)inv[i+1]=mod-inv[mod%(i+1)]*(mod/(i+1))%mod;
+        invfact[i+1]=invfact[i]*inv[i+1]%mod;
     }
-    for (int j = 1; j < v.size(); j++) {
-        for (int k = 1; k < j; k++) {
-            v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
-            if(mod>0) v[j][k] %= mod;
-        }
-    }
-    return v;
 }
-vector<vector<long long>> combi;
-
+ll getCombination(ll n,ll k,ll mod){
+    if(n<0||k<0||k>n)return 0;
+    return (fact[n]*invfact[n-k]%mod)*invfact[k]%mod;
+}
+ll getCombinationH(ll n, ll k , ll mod){
+    return getCombination(n+k-1,k,mod);
+}
+ll getMod(ll value,ll mod){
+    if(value == 0) return 0;
+    if(mod==0) return -1;
+    value %= mod;
+    if(value<0) value += mod;
+    return value;
+}
 int main(){
 
-    cin >> N >> K;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-    ll ans;
-    combi = combination(2010,2010,MOD);
-    for( int sousa = 1 ; sousa <= K  ; sousa++ ){
-        ans = 1;
-        ans *= combi[N-K+1][sousa];
-        ans *= combi[K-1][sousa-1];
-        cout << ans % MOD << endl;
+    cin >> N;
+    cin >> K;
+
+    setupCombination(2010,MOD);
+    
+    for( ll ki = 1 ; ki <=K ; ki++ ){
+        int betB = ki - 1;
+        ll ans = getCombination(K-1,betB,MOD);
+
+        int l = betB+2;
+        int r = (N-K)-betB;
+
+        ans *= getCombinationH(l,r,MOD);
+
+        cout << getMod(ans,MOD) << endl;
     }
-
-
-
-
 
 }
