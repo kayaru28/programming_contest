8ad8588cba2bac7d53437aa89e78a34c8f36e31c
kayaru28 <istorytale090415@gmail.com>
2020-11-01 18:58:33 +0900

    update abc134 C

--- old_contest/at_coder_abc134_C.cpp
+++ old_contest/at_coder_abc134_C.cpp
@@ -1,14 +1,25 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
-#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
 #define INF 1e9
 #define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
 #define MOD 1000000007
 #define pb push_back
 #define ll long long
+#define ld long double
 #define ull unsigned long long
 #define vint vector<int>
 #define vll vector<ll>
@@ -16,42 +27,51 @@ using namespace std;
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
+
+vll A;
+ll N;
+
+ll ltmp;
+string stmp;
+double dtmp;
 
 int main(){
 
-    int N;
-    cin >> N;
-    int A[N];
-    
-    rep(ni,N) cin >> A[ni];
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-    int n1=0;
-    int n2=1;
-    if(A[n1]<A[n2]){
-        n1 = 1;
-        n2 = 0;
+    cin >> N;
+    rep(ni,N){
+        cin >> ltmp;
+        A.push_back(ltmp);
     }
-    for( int ni = 2 ; ni <N ; ni++ ){
-        if(A[ni]>A[n1]){
-            n2 = n1;
-            n1 = ni;
-        }else if(A[ni] > A[n2]){
-            n2 = ni;
+
+    ll Amax=A[0];
+    ll Asec=0;
+
+    for( ll ni = 1 ; ni <N ; ni++ ){
+        ltmp = A[ni];
+        if(Amax<ltmp){
+            Asec = Amax;
+            Amax = ltmp;
+        }else if(Asec<ltmp){
+            Asec = ltmp;
         }
     }
+
     rep(ni,N){
-        if(ni==n1) cout << A[n2] << endl;
-        else cout << A[n1] << endl;
+        if(A[ni]==Amax) cout << Asec << endl;
+        else cout << Amax << endl;
     }
 
+    
 
 }
