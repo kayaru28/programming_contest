adc8dcac63e410168de0bfc7a7b5fa477a99177b
kayaru28 <istorytale090415@gmail.com>
2020-11-01 18:58:39 +0900

    update abc134 D

--- old_contest/at_coder_abc134_D.cpp
+++ old_contest/at_coder_abc134_D.cpp
@@ -1,14 +1,25 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
-#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
 #define INF 1e9
 #define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
 #define MOD 1000000007
 #define pb push_back
 #define ll long long
+#define ld long double
 #define ull unsigned long long
 #define vint vector<int>
 #define vll vector<ll>
@@ -16,47 +27,73 @@ using namespace std;
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
 
+vll A;
+ll N;
+
+ll ltmp;
+string stmp;
+double dtmp;
+int flag[base10_6]={};
+vector<ll> prime_numbers;
+void getPrimeNumbers(ll n_max){
+    bool is_prime[n_max+1];
+    for( ll ni = 0 ; ni <=n_max ; ni++ ){
+        is_prime[ni]=true;
+    }
+   is_prime[0]=is_prime[1]=false;
+    for( ll number = 2 ; number <=n_max ; number++ ){
+        if(is_prime[number]){
+            prime_numbers.push_back(number);
+            for( ll ni = number*2 ; ni <=n_max ; ni+=number ){
+                is_prime[ni]=false;
+            }
+        }
+    }
+}
 int main(){
 
-    int N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    getPrimeNumbers(base10_6);
+
     cin >> N;
-    int A[N+10];
-    int B[N+10];
-    
-    for( int ni = 1 ; ni <=N ; ni++ ){
-        cin >> A[ni];
+    A.push_back(0);
+    rep(ni,N){
+        cin >> ltmp;
+        A.push_back(ltmp);
     }
-    A[N+1]=0;
-    B[N+1]=0;
 
-    int sum;
-    int cnt;
-    int M=0;
-    for( int ni = N ; ni >=1 ; ni-- ){
-        sum = 0;
-        cnt = 2;
-        while(cnt * ni <= N){
-            sum += B[cnt * ni];
-            cnt++;
+    for( ll ni = N ; ni >=1 ; ni-- ){
+        ll cnt = A[ni];
+        //ll index = 0;
+        //while(ni*prime_numbers[index]<=N){
+        ll index = 2;
+        while(ni*index<=N){
+        
+            ll val = ni*index;
+            cnt += flag[val];
+            index++;
         }
-        B[ni] = ( sum + A[ni] ) % 2;
-        M+=B[ni];
-    }
-    cout << M << endl;
-    for( int ni = 1 ; ni <=N ; ni++ ){
-        if(B[ni]==1) cout << ni << endl;;
+
+        flag[ni] = (cnt%2);
     }
 
-
-
+    ll csum=0;
+    for( ll ni = 1 ; ni <=N ; ni++ ){
+        csum += flag[ni];
+    }
+    cout << csum << endl;
+    for( ll ni = 1 ; ni <=N ; ni++ ){
+        if(flag[ni]==1) cout << ni << " ";
+    }
 }
