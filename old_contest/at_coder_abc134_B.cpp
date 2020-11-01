6e8757550da2711ddc96d73eb1e1def6fc3fe300
kayaru28 <istorytale090415@gmail.com>
2020-11-01 18:58:26 +0900

    update abc134 B

--- old_contest/at_coder_abc134_B.cpp
+++ old_contest/at_coder_abc134_B.cpp
@@ -1,14 +1,25 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
-#define rep(i,n) for (int i = 0; i < (n) ; i++)
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
 #define INF 1e9
 #define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
 #define MOD 1000000007
 #define pb push_back
 #define ll long long
+#define ld long double
 #define ull unsigned long long
 #define vint vector<int>
 #define vll vector<ll>
@@ -16,27 +27,42 @@ using namespace std;
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
+
+ll A;
+ll B;
+ll D;
+ll N;
+ll M;
+ll K;
+
+ll ltmp;
+string stmp;
+double dtmp;
 
 int main(){
 
-    int A;
-    int B;
-    int C;
-    int N;
-    
-    cin >> A;
-    cin >> B;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-    if(A % (1 + 2 * B)==0)    cout << A / (1 + 2*B) << endl; 
-    else cout << A / (1 + 2*B) + 1 << endl;    
+    cin >> N;
+    cin >> D;
+
+    D = 2 * D + 1;
+    if(N%D==0){
+        cout << N/D << endl;
+    }else{
+        cout << N/D+1 << endl;
+
+    }
+
+    
 
 }
