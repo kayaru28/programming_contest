e5bbe6c32bb4373da1490da7feafa5583461b795
kayaru28 <istorytale090415@gmail.com>
2021-01-07 00:01:03 +0900

    update

--- old_contest/at_coder_abc102_C.cpp
+++ old_contest/at_coder_abc102_C.cpp
@@ -1,68 +1,96 @@
 #include  <iostream>
 #include  <stdio.h>
 #include <algorithm>
+#include <map>
+#include <math.h>
+
 using namespace std;
 #include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
 //#include <stack>
 //#include <queue>
 
-/*
-#include <math.h>
-int standerd = int(pow(10.0,9.0)) + 7;
-*/
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
 string ans_Yes = "Yes"; 
 string ans_No = "No"; 
 string ans_yes = "yes"; 
 string ans_no = "no"; 
- 
 
+vll A;
+vll Ai;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
+
+ll ltmp;
+string stmp;
+double dtmp;
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+
+ll getval(ll val){
+    ll sum = 0;
+    rep(ni,N) sum+= abs(Ai[ni]-val);
+
+    return sum;
+}
 int main(){
 
-    int N;
-    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
     cin >> N;
 
-    vector<int> A;
-    for( int ni = 0 ; ni < N ; ni++ ){
-        int tmp;
-        cin >> tmp;
-        A.push_back(tmp - (ni + 1) );
+    rep(ni,N) A.push_back(0);
+    rep(ni,N) cin >> A[ni];
+
+    rep(ni,N) Ai.push_back(A[ni]-(ni+1));
+
+    sort(Ai.begin(),Ai.end());
+
+    vll sumi;
+    sumi.push_back(0);
+    for( ll ni = 0 ; ni <N ; ni++ ){
+        sumi.push_back(sumi[ni]+Ai[ni]);
     }
-    sort(A.begin(),A.end());
 
-    int index;
-    int b;
-    long long score;
-    long long ans;
-    if(N==1){
-        ans = 0;
-    }else{
-        if(N%2==0){
-            index = N /2 - 1;
-            b = A[index];
-            score=0;
-            for( int ni = 0 ; ni < N ; ni++ ){
-                score += abs(A[ni]-b);
-            }
-            ans = score;
-
-            index = N /2 - 1 + 1;
-            b = A[index];
-            score=0;
-            for( int ni = 0 ; ni < N ; ni++ ){
-                score += abs(A[ni]-b);
-            }
-            ans = min(ans,score);
-
-        }else{
-            index = (N + 1) /2 - 1;
-            b = A[index];
-            score=0;
-            for( int ni = 0 ; ni < N ; ni++ ){
-                score += abs(A[ni]-b);
-            }
-            ans = score;
-        }
+    ll ans = llINF;
+    for( ll ni = 1 ; ni <=N ; ni++ ){
+        ll base = Ai[ni-1];
+        ll li = abs(sumi[ni-1] - base * (ni-1));
+        ll ri = abs((sumi[N]-sumi[ni]) - base * (N-ni));
+        ll tmpans = li + ri;
+        //cout << ni <<" "<< li << " "<< ri << endl;
+        ans = llmin(ans,tmpans);
     }
     cout << ans << endl;
 
