3ccee5adb608f32e34cd3b1c1454f028ab91f11f
kayaru28 <istorytale090415@gmail.com>
2021-02-04 00:13:01 +0900

    add

---
+++ old_contest/at_coder_abc141_E.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<iomanip>
+#include<math.h>
+#include<complex>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<map>
+#include<set>
+#include<bitset>
+#include<functional>
+#include<assert.h>
+#include<numeric>
+
+using namespace std;
+#include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+
+//#include <stack>
+//#include <queue>
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+/*
+sort(ord.begin(),ord.end(),[&](int x, int y){
+    return p[x]>p[y];
+});
+*/
+
+string ans_Yes = "Yes"; 
+string ans_No = "No"; 
+string ans_yes = "yes"; 
+string ans_no = "no"; 
+
+ll A;
+ll B;
+ll C;
+ll N;
+ll M;
+ll K;
+
+ll ltmp;
+string stmp;
+double dtmp;
+ll ans = 0;
+string S;
+map<string,ll> mapmin;
+map<string,ll> mapmax;
+map<string,ll> mapval;
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+vll tmpans;
+void zAlgorithm(string S) {
+    ll ci = 0;
+    vll ansi;
+    ansi.resize(S.size());
+    for (ll i = 1; i < S.size(); i++){
+        if (i+ansi[i-ci] < ci+ansi[ci]) {
+            ansi[i] = ansi[i-ci];
+        } else {
+            ll j = ci+ansi[ci]-i;
+            if(j<0) j = 0;
+            while (i+j < S.size() && S[j] == S[i+j]) ++j;
+            ansi[i] = j;
+        }
+    }
+    ansi[0] = S.size();
+
+    rep(si,S.size()) tmpans.push_back(llmin(si,ansi[si]));
+}
+
+    int main(){
+        int n;
+        string s;
+        cin >> n >> s;
+        mti dp(n+1,vi(n+1,0));
+        int ma=0;
+        rep(i,n){
+            rep(j,n){
+                dp[i+1][j+1]=(s[i]==s[j]?dp[i][j]+1:0);
+                ma=max(ma,min(dp[i+1][j+1], abs(i-j)));
+            }
+        }
+        cout << ma << endl;
+        return 0;
+    }
