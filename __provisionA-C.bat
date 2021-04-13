537e88deb113a895fc1838d4894223ff918bc0eb
kayaru28 <istorytale090415@gmail.com>
2021-04-13 15:14:41 +0900

    add : tool

---
+++ __provisionA-C.bat
@@ -0,0 +1,5 @@
+set /P CONTEST="input contest(ex.abc170): "
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_A.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_B.cpp
+copy /-Y at_coder_abc1XX_.cpp at_coder_%CONTEST%_C.cpp
+set /P USR_INPUT_STR="finished(input something): "
