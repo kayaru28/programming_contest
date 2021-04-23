233b22558194dc8470f7700b5b16f07b1c19147f
kayaru28 <istorytale090415@gmail.com>
2021-04-23 23:52:31 +0900

    add

---
+++ old_contest/at_coder_jsc2021_D.cpp
@@ -0,0 +1,106 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+
+using namespace std;
+#include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007 //10^9+7
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+
+
+//#include <stack>
+//#include <queue>
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+string ans_Yes = "Yes"; 
+string ans_No = "No"; 
+string ans_yes = "yes"; 
+string ans_no = "no"; 
+
+ll A;
+ll B;
+ll C;
+ll N;
+ll P;
+ll K;
+ll T;
+ll X;
+ll Y;
+ll Z;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+ll getMod(ll value,ll mod=MOD){
+    if(value == 0) return 0;
+    if(mod==0) return -1;
+    value %= mod;
+    if(value<0) value += mod;
+    return value;
+}
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+ll getPower(ll value,ll shisu,ll mod){
+    ll ans = 1;
+    ll cnt = shisu;
+    while(cnt>0){
+        if(cnt%2==1){
+            ans = ans * value % mod;
+        }
+        value = value * value % mod;
+        cnt = cnt/2;
+    }
+    return ans;
+}
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    cin >> P;
+
+    ll kouhoP = P-1;
+    ll generate = kouhoP-1;
+
+    ll ans = kouhoP;
+    ans = getMod(ans);
+    ans = ans * getPower(generate,N-1,MOD);
+    ans = getMod(ans);
+    cout << ans << endl;
+
+}
