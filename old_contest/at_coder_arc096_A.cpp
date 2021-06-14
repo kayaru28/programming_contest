7a8efe74f6f60253d29ce40da768add4cf64bfe2
kayaru28 <istorytale090415@gmail.com>
2021-06-15 01:47:27 +0900

    add

---
+++ old_contest/at_coder_arc096_A.cpp
@@ -0,0 +1,119 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+
+
+
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> X;
+    cin >> Y;
+
+    ll ans = A*X + B*Y;
+
+    ll Cpiza;
+    ll tmpans;
+
+    Cpiza = llmin(X,Y);
+    tmpans = Cpiza * C * 2;
+    tmpans += ((X - Cpiza) * A);
+    tmpans +=  (Y - Cpiza) * B;
+
+    ans = llmin(ans,tmpans);
+
+    Cpiza = llmax(X,Y);
+    tmpans = Cpiza * C * 2;
+
+    ans = llmin(ans,tmpans);
+
+    cout << ans << endl;
+
+
+
+    
+
+}
