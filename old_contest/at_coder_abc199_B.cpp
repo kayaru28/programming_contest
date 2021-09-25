a4eccd37d414545afa49f5f9af0ff7203eec43ee
kayaru28 <istorytale090415@gmail.com>
2021-09-26 00:14:11 +0900

    update

---
+++ old_contest/at_coder_abc199_B.cpp
@@ -0,0 +1,89 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+
+using namespace std;
+#include <vector>
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+
+
+//#include <stack>
+//#include <queue>
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+string ans_Yes = "Yes"; 
+string ans_No = "No"; 
+string ans_yes = "yes"; 
+string ans_no = "no"; 
+
+vll A;
+vll B;
+ll C;
+ll N;
+ll M;
+ll K;
+ll T;
+ll X;
+ll Y;
+ll Z;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    rep(ni,N)  cin >> A[ni];
+    rep(ni,N)  cin >> B[ni];
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+
+    ll ans = B[0]-A[N-1]+1;
+
+    cout << llmax(0,ans) << endl; 
+    
+
+}
