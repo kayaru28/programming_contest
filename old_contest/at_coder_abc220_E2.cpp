b699b45255d4e5a7d968e76e4f3f748c6359d7da
kayaru28 <istorytale090415@gmail.com>
2021-10-16 00:38:59 +0900

    add

---
+++ old_contest/at_coder_abc220_E2.cpp
@@ -0,0 +1,161 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define rep2(i,n,i2,n2) for (ll i = 0; i < (n) ; i++) for (ll i2 = 0; i2 < (n2) ; i2++)
+#define incRepFT(i,s,n) for (ll i = (s); i <= (n) ; i++)
+#define decRepFT(i,s,n) for (ll i = (s); i >= (n) ; i--)
+
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 998244353
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+typedef pair<ll,ll> P;  
+
+
+ll getMod(ll value,ll mod=MOD){
+    if(value == 0) return 0;
+    if(mod==0) return -1;
+    value %= mod;
+    if(value<0) value += mod;
+    return value;
+}
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+// char t - 'a'
+// char t - '0'
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z;
+
+string S;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+P d_move[4] = {
+    P(0 , 1),P(0 , -1),P(1 , 0),P(-1 , 0)//,P(1 , 1),P(1 , -1),P(-1 , 1),P(-1 , -1)
+};
+//for(P drc : d_move)
+ll getPower(ll value,ll shisu,ll mod=MOD){
+    ll ans = 1;
+    ll cnt = shisu;
+    while(cnt>0){
+        if(cnt%2==1){
+            ans = ans * value % mod;
+        }
+        value = value * value % mod;
+        cnt = cnt/2;
+    }
+    return ans;
+}
+
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> D;
+
+    ll maxl_f1 = N;
+
+
+    ll slen = 0;
+    ll llen = D;
+    ll ans = 0;
+    while(slen <= llen){
+        ll l_bottom = N - llen;
+        if(l_bottom >= 0){
+            ll cnt_top = getPower(2,l_bottom)-1;
+            cnt_top = getMod(cnt_top);
+
+            //ll l_cnt = getPower(2,llen);
+            //ll s_cnt = getPower(2,slen);
+
+            ll tmpans = cnt_top;
+            //tmpans *= l_cnt;
+            //tmpans *= s_cnt;
+
+            if(slen==llen){
+                tmpans *= getPower(2,D+1 -2);
+            }else if(slen>0){
+                tmpans *= getPower(2,D+1-1);
+            }else{
+                tmpans *= getPower(2,D+1);
+            }
+            //cout << llen << " " << slen << " " << tmpans << endl;
+            ans += tmpans;
+            ans = getMod(ans);
+        }
+        slen++;
+        llen--;
+
+    }
+    cout << ans << endl;
+
+
+
+
+}
