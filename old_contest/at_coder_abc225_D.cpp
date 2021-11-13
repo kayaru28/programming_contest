0a8678db38bdb8bd3a6daa22812512f92683782b
kayaru28 <istorytale090415@gmail.com>
2021-11-14 02:46:29 +0900

    add

---
+++ old_contest/at_coder_abc225_D.cpp
@@ -0,0 +1,150 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define rep2(i,n,i2,n2) for (ll i = 0; i < (n) ; i++) for (ll i2 = 0; i2 < (n2) ; i2++)
+#define incRepFT(i,s,n) for (ll i = (s); i <= (n) ; i++)
+#define decRepFT(i,s,n) for (ll i = (s); i >= (n) ; i--)
+
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+typedef pair<ll,ll> P;  
+
+
+
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+// char t - 'a'
+// char t - '0'
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z,Q;
+vll As,Bs,Cs;
+
+string S;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+P d_move[4] = {
+    P(0 , 1),P(0 , -1),P(1 , 0),P(-1 , 0)//,P(1 , 1),P(1 , -1),P(-1 , 1),P(-1 , -1)
+};
+//for(P drc : d_move)
+vvll masu;
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    cin >> Q;
+
+
+    As.resize(Q);
+    Bs.resize(Q);
+    Cs.resize(Q);
+    rep(Qi,Q){
+        cin >> As[Qi] >> Bs[Qi];
+        if(As[Qi] <= 2) cin >> Cs[Qi];
+    }
+
+    ll backi[N+10] = {};
+    ll fronti[N+10] = {};
+    rep(qi,Q){
+        if(As[qi]==1){
+            backi[Bs[qi]] = Cs[qi];
+            fronti[Cs[qi]] = Bs[qi];
+        }else if(As[qi]==2){
+            backi[Bs[qi]] = 0;
+            fronti[Cs[qi]] = 0;
+        }else{
+            int index = Bs[qi];
+            while(fronti[index]!=0){
+                index = fronti[index];
+            }
+
+            vll ans;
+            ans.push_back(index);
+            while(backi[index]!=0){
+                index = backi[index];
+                ans.push_back(index);
+            }
+            cout << ans.size() << " ";
+            for(ll ai : ans ) cout << ai << " ";
+            cout << endl;
+        }
+        
+
+
+    }
+
+
+
+  
+
+}
