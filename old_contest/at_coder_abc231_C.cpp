d2722de1b06b382d62edb6d660b39f8f51ba8c17
kayaru28 <istorytale090415@gmail.com>
2022-02-09 00:53:46 +0900

    add

---
+++ old_contest/at_coder_abc231_C.cpp
@@ -0,0 +1,153 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define rep2(i,n,i2,n2) for (ll i = 0; i < (n) ; i++) for (ll i2 = 0; i2 < (n2) ; i2++)
+#define incRepFT(i,s,n) for (ll i = (s); i <= (n) ; i++)
+#define decRepFT(i,s,n) for (ll i = (s); i >= (n) ; i--)
+
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+typedef pair<ll,ll> P;  
+
+
+
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+// char t - 'a'
+// char t - '0'
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z,Q;
+vll As,Bs,Cs;
+vll Xs;
+string S;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+P d_move[4] = {
+    P(0 , 1),P(0 , -1),P(1 , 0),P(-1 , 0)//,P(1 , 1),P(1 , -1),P(-1 , 1),P(-1 , -1)
+};
+//for(P drc : d_move)
+vvll masu;
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+ll getans(ll x){
+    if(x <= Bs[0]) return Cs[0];
+    ll blasti = Bs.size()-1;
+    if(x == Bs[blasti]) return Cs[blasti];
+    if(x > Bs[blasti]) return 0;
+    
+    ll li = 0;
+    ll ri = blasti;
+    while(li+1 < ri){
+        ll mid = (li+ri)/2;
+
+        if(Bs[mid]==x) return Cs[mid];
+        else if(Bs[mid]<x){
+            li = mid;
+        }else{
+            ri = mid;
+        }
+    }
+
+    return Cs[ri];
+
+}
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    cin >> Q;
+
+    As.resize(N);
+    rep(ni,N) cin >> As[ni];
+
+
+    Xs.resize(Q);
+    rep(qi,Q) cin >> Xs[qi];
+
+    map<ll,ll> mapsum;
+    rep(ni,N) mapsum[As[ni]]++;
+
+    for(auto x : mapsum){
+        Bs.push_back(x.first);
+        Cs.push_back(x.second);
+    }
+    ll bef = 0;
+    for( ll ci = Cs.size()-1-1 ; ci >=0 ; ci-- ){
+        Cs[ci] += Cs[ci+1];
+    }
+
+    rep(qi,Q){
+        cout << getans(Xs[qi]) << endl;
+    }
+    
+
+}
