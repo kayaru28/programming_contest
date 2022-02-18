bf0d58df616d3ed66506e2390b4225051fb38a96
kayaru28 <istorytale090415@gmail.com>
2022-02-18 23:55:54 +0900

    add

---
+++ old_contest/at_coder_abc232_D.cpp
@@ -0,0 +1,138 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define rep2(i,n,i2,n2) for (ll i = 0; i < (n) ; i++) for (ll i2 = 0; i2 < (n2) ; i2++)
+#define incRepFT(i,s,n) for (ll i = (s); i <= (n) ; i++)
+#define decRepFT(i,s,n) for (ll i = (s); i >= (n) ; i--)
+
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 1000000007
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+typedef pair<ll,ll> P;  
+
+
+
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+// char t - 'a'
+// char t - '0'
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z;
+vll As,Bs;
+
+string S;
+
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+P d_move[4] = {
+    P(0 , 1),P(0 , -1),P(1 , 0),P(-1 , 0)//,P(1 , 1),P(1 , -1),P(-1 , 1),P(-1 , -1)
+};
+//for(P drc : d_move)
+vstr masu;
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+
+
+map<ll,ll> memoV;
+ll getkey(ll hi, ll wi){
+    return hi * 1000 + wi;
+}
+
+
+
+ll getans(ll hi, ll wi){
+    ll key = getkey(hi,wi);
+    if(masu[hi][wi]=='#') return 0;
+    if(memoV[key]>0) return memoV[key];
+
+    ll v1 = 0;
+    ll v2 = 0;
+
+    if(hi+1 < H) v1 = getans(hi+1,wi);
+    if(wi+1 < W) v2 = getans(hi,wi+1);
+
+    ll tmpans =llmax(v1,v2) + 1;
+    memoV[key] = tmpans;
+    return tmpans;
+}
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> H >> W;
+    masu.resize(H);
+    rep(hi,H){
+        cin >> masu[hi];
+    }
+
+    cout << getans(0,0) << endl;
+
+    
+
+}
