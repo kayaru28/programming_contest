1b11dda4065592b336a4782355207afb1755d035
kayaru28 <istorytale090415@gmail.com>
2022-02-21 21:41:54 +0900

    add

---
+++ old_contest/at_coder_abc232_E.cpp
@@ -0,0 +1,163 @@
+
+
+
+#include  <iostream>
+#include  <stdio.h>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+#define rep(i,n) for (ll i = 0; i < (n) ; i++)
+#define rep2(i,n,i2,n2) for (ll i = 0; i < (n) ; i++) for (ll i2 = 0; i2 < (n2) ; i2++)
+#define incRepFT(i,s,n) for (ll i = (s); i <= (n) ; i++)
+#define decRepFT(i,s,n) for (ll i = (s); i >= (n) ; i--)
+
+#define INF 1e9
+#define llINF 1e18
+#define base10_4 10000      //1e4
+#define base10_5 100000     //1e5
+#define base10_6 1000000    //1e6
+#define base10_7 10000000   //1e7
+#define base10_8 100000000  //1e8
+#define base10_9 1000000000 //1e9
+
+#define MOD 998244353
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vint vector<int>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+typedef pair<ll,ll> P;  
+
+
+
+
+// #include <iomanip>
+//  cout << fixed << setprecision(15) << y << endl;
+
+// for(char c : S)
+
+//min({a1, a2, ..., an})
+//max({a1, a2, ..., an})
+//swap(a, b)
+//S.substr(si)
+// sort <--> reverse
+//count(v.begin(), v.end(), x) 
+// char t - 'a'
+// char t - '0'
+
+//xを2進数にした時の1の数
+//__builtin_popcount(x) 
+//__builtin_popcountll(x) 
+
+ll A;
+ll B;
+ll C;
+ll D;
+ll N;
+ll M;
+ll K;
+ll T;
+ll H;
+ll W;
+ll X;
+ll Y;
+ll Z;
+vll As,Bs;
+
+string S;
+
+ll getMod(ll value,ll mod=MOD){
+    if(value == 0) return 0;
+    if(mod==0) return -1;
+    value %= mod;
+    if(value<0) value += mod;
+    return value;
+}
+ll ltmp;
+string stmp;
+double dtmp;
+
+ll x1,x2,yi1,yi2;
+
+ll llmin(ll a,ll b){
+    if(a>=b) return b;
+    return a;
+}
+ll llmax(ll a,ll b){
+    if(a<=b) return b;
+    return a;
+}
+P d_move[4] = {
+    P(0 , 1),P(0 , -1),P(1 , 0),P(-1 , 0)//,P(1 , 1),P(1 , -1),P(-1 , 1),P(-1 , -1)
+};
+//for(P drc : d_move)
+vvll masu;
+double double_hosei = 1000000; //求められる精度分補正をかけておく
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> H >>W >> K;
+    cin >> x1 >> yi1 >> x2 >> yi2;
+
+
+    ll cnt_1 = 1;
+    ll cnt_lineH1 = 0;
+    ll cnt_lineW1 = 0;
+    ll cnt_notline1 = 0;
+    ll ki = 1;
+    ll modHW = getMod(H*W);
+    ll flag1 = 1;
+    while(ki <= K){
+
+        ll tmp_cnt_1 = getMod(cnt_lineH1 * (H-1)) + getMod(cnt_lineW1 * (W-1));
+        ll tmp_cnt_lineH1 = cnt_1 + getMod(cnt_lineH1 * (H - 1 - flag1));
+        tmp_cnt_lineH1 = getMod(tmp_cnt_lineH1) + getMod( cnt_notline1 * (W-1));
+        ll tmp_cnt_lineW1 = cnt_1 + getMod(cnt_lineW1 * (W - 1 - flag1));
+        tmp_cnt_lineW1 = getMod(tmp_cnt_lineW1) + getMod( cnt_notline1 * (H-1));
+
+        ll tmp_notline1 = getMod(cnt_notline1 * (H -2) ) + cnt_lineW1;
+        tmp_notline1 = getMod(tmp_notline1) + getMod(cnt_notline1 * (W -2) ) + cnt_lineH1;
+
+        cnt_1=getMod(tmp_cnt_1);
+        cnt_lineH1=getMod(tmp_cnt_lineH1);
+        cnt_lineW1=getMod(tmp_cnt_lineW1);
+        cnt_notline1=getMod(tmp_notline1);
+        ki ++;
+    }
+
+    ll ans;
+    if(x1==x2 && yi1==yi2){
+        ans = cnt_1;
+    }else if(x1==x2){
+        ans = cnt_lineW1;
+    }else if(yi1==yi2){
+        ans = cnt_lineH1;
+    }else{
+        ans = cnt_notline1;
+    }
+    //cout << cnt_1 << endl;
+    //cout << cnt_lineH1 << endl;
+    //cout << cnt_lineW1 << endl;
+    //cout << cnt_notline1 << endl;
+    cout << ans << endl;
+
+
+
+
+    
+
+}
